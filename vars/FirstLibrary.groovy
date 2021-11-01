fe33d126e39f3d5746f19f8640ebb21220c515a9
XQX <1730270966@qq.com>
2021-11-01 16:45:30 +0800

    fix

--- vars/FirstLibrary.groovy
+++ vars/FirstLibrary.groovy
@@ -1,6 +1,6 @@
 #! /usr/bin/env groovy
 
-def call(String name == "xiaqian", Date date = new Date()) {
+def call(String name = "xiaqian", Date date = new Date()) {
     echo "Hello, ${name}!"
     echo "Today is ${date}"
 }
